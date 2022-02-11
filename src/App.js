7d1f2a38b4532bb7fa08a4ba9dfd0da2d4646abb
Vinod Hansda <vinod.hansda@dreamztech.com>
2022-02-11 18:32:10 +0530

    feat:update app js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,16 @@ function App() {
        
         <ul className='nav'>
           <li className='nav-item'>
+            <Link className='nav-link' to='/login'>Login</Link>
+          </li>
+          <li className='nav-item'>
             <Link className='nav-link' to='/'>Home</Link>
           </li>
           <li className='nav-item'>
             <Link className='nav-link' to='/counter'>Counter</Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/register'>Register</Link>
           </li>
           <li className='nav-item'>
             <Link className='nav-link' to='/fruits'>Fruits</Link>
